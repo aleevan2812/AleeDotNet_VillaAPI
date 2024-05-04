531323d6a421ceb074e7c895baf0e4680aefdc23
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-05-04 10:30:37 +0700

    03 - 012 Content Negotiations

--- AleeDotNet_VillaAPI/Program.cs
+++ AleeDotNet_VillaAPI/Program.cs
@@ -3,7 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 // AddNewtonsoftJson() is used for MVC.NewtonsoftJson package
-builder.Services.AddControllers().AddNewtonsoftJson();
+// AddXmlDataContractSerializerFormatters() is used for supporting XML formating
+builder.Services.AddControllers(option =>
+{
+	option.ReturnHttpNotAcceptable = true;
+	// if a format is not acceptable, return the appropriate error message
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
