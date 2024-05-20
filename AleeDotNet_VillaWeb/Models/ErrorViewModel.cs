aa4cec0327348d99a81ba326deffde23f7e8a329
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-05-20 21:47:45 +0700

    7 - 001 Setup MVC Web Project

---
+++ AleeDotNet_VillaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AleeDotNet_VillaWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
