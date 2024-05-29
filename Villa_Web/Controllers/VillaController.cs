3833366c73994e65bf113fcbcf2e267cc8b25096
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-05-29 22:38:56 +0700

    07 - 011 Update Villa Action Methods

--- Villa_Web/Controllers/VillaController.cs
+++ Villa_Web/Controllers/VillaController.cs
@@ -9,8 +9,9 @@ namespace Villa_Web.Controllers;
 
 public class VillaController : Controller
 {
-    private readonly IVillaService _villaService;
     private readonly IMapper _mapper;
+    private readonly IVillaService _villaService;
+
     public VillaController(IVillaService villaService, IMapper mapper)
     {
         _villaService = villaService;
@@ -23,12 +24,10 @@ public class VillaController : Controller
 
         var response = await _villaService.GetAllAsync<APIResponse>();
         if (response != null && response.IsSuccess)
-        {
             list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
-        }
         return View(list);
     }
-    
+
     public async Task<IActionResult> CreateVilla()
     {
         return View();
@@ -40,13 +39,35 @@ public class VillaController : Controller
     {
         if (ModelState.IsValid)
         {
-
             var response = await _villaService.CreateAsync<APIResponse>(model);
-            if (response != null && response.IsSuccess)
-            {
-                return RedirectToAction(nameof(IndexVilla));
-            }
+            if (response != null && response.IsSuccess) return RedirectToAction(nameof(IndexVilla));
         }
+
+        return View(model);
+    }
+
+    public async Task<IActionResult> UpdateVilla(int villaId)
+    {
+        var response = await _villaService.GetAsync<APIResponse>(villaId);
+        if (response != null && response.IsSuccess)
+        {
+            var model = JsonConvert.DeserializeObject<VillaDTO>(Convert.ToString(response.Result));
+            return View(_mapper.Map<VillaUpdateDTO>(model));
+        }
+
+        return NotFound();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> UpdateVilla(VillaUpdateDTO model)
+    {
+        if (ModelState.IsValid)
+        {
+            var response = await _villaService.UpdateAsync<APIResponse>(model);
+            if (response != null && response.IsSuccess) return RedirectToAction(nameof(IndexVilla));
+        }
+
         return View(model);
     }
 }
