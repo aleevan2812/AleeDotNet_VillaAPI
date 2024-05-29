8da3d2bc6ca7cedcb02c836378c96e02fb757f10
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-05-29 23:15:05 +0700

    07 - 015 Getting Villa Numbers

--- Villa_Web/Models/Dto/VillaNumberDTO.cs
+++ Villa_Web/Models/Dto/VillaNumberDTO.cs
@@ -6,6 +6,6 @@ public class VillaNumberDTO
 {
     [Required] public int VillaNo { get; set; }
     [Required] public int VillaID { get; set; }
-
+    public VillaDTO Villa { get; set; }
     public string SpecialDetails { get; set; }
 }
