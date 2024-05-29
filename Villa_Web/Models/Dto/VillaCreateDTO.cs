7342617030dba1b743dfbe11b0c0f8d6ecc567c7
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-05-29 16:37:51 +0700

    07 - 009 Create Villa UI

--- Villa_Web/Models/Dto/VillaCreateDTO.cs
+++ Villa_Web/Models/Dto/VillaCreateDTO.cs
@@ -6,10 +6,10 @@ namespace Villa_Web.Models.Dto;
 public class VillaCreateDTO
 {
     [Required] [MaxLength(30)] public string Name { get; set; }
-    public string Details { get; set; }
+    public string? Details { get; set; }
     public int Occupancy { get; set; }
     public int Sqft { get; set; } // square feet^2
     [Required] public double Rate { get; set; }
-    public string ImageUrl { get; set; }
-    public string Amenity { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? Amenity { get; set; }
 }
