0da905876629cffcb2bd04640b332b23c71d939e
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-05-30 14:04:43 +0700

    08 - 005 Generate Token on Successful Login

--- Villa_API/Repository/UserRepository.cs
+++ Villa_API/Repository/UserRepository.cs
@@ -1,4 +1,8 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Villa_API.Data;
 using Villa_API.Models;
 using Villa_API.Models.Dto;
@@ -9,10 +13,12 @@ namespace Villa_API.Repository;
 public class UserRepository : IUserRepository
 {
     private readonly ApplicationDbContext _db;
+    private string secretKey;
 
-    public UserRepository(ApplicationDbContext db)
+    public UserRepository(ApplicationDbContext db, IConfiguration configuration)
     {
         _db = db;
+        secretKey = configuration.GetValue<string>("ApiSettings:Secret");
     }
 
     public bool IsUniqueUser(string username)
@@ -23,9 +29,35 @@ public class UserRepository : IUserRepository
         return false;
     }
 
-    public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+    public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
     {
-        throw new NotImplementedException();
+        var user = _db.LocalUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDTO.UserName.ToLower()
+                                                      && u.Password == loginRequestDTO.Password);
+
+        if (user == null) return null;
+
+        //if user was found generate JWT Token
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(secretKey);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, user.Id.ToString()),
+                new Claim(ClaimTypes.Role, user.Role)
+            }),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        LoginResponseDTO loginResponseDTO = new LoginResponseDTO()
+        {
+            Token = tokenHandler.WriteToken(token),
+            User = user
+        };
+        return loginResponseDTO;
     }
 
     public async Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
