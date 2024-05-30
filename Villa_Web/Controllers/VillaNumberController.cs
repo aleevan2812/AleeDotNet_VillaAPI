ad8ef31303403770068e9f5516a8503739c03f43
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-05-30 09:13:59 +0700

    07 - 021 Create Villa Number GET

--- Villa_Web/Controllers/VillaNumberController.cs
+++ Villa_Web/Controllers/VillaNumberController.cs
@@ -1,8 +1,10 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using Villa_Web.Models;
 using Villa_Web.Models.Dto;
+using Villa_Web.Models.VM;
 using Villa_Web.Services.IServices;
 
 namespace Villa_Web.Controllers;
@@ -11,11 +13,13 @@ public class VillaNumberController : Controller
 {
     private readonly IVillaNumberService _villaNumberService;
     private readonly IMapper _mapper;
+    private readonly IVillaService _villaService;
 
-    public VillaNumberController(IVillaNumberService villaNumberService, IMapper mapper)
+    public VillaNumberController(IVillaNumberService villaNumberService, IMapper mapper, IVillaService villaService)
     {
         _villaNumberService = villaNumberService;
         _mapper = mapper;
+        _villaService = villaService;
     }
 
     public async Task<IActionResult> IndexVillaNumber()
@@ -29,5 +33,37 @@ public class VillaNumberController : Controller
         }
 
         return View(list);
+    }
+    
+    public async Task<IActionResult> CreateVillaNumber()
+    {
+        VillaNumberCreateVM villaNumberVM = new();
+        var response = await _villaService.GetAllAsync<APIResponse>();
+        if (response != null && response.IsSuccess)
+        {
+            villaNumberVM.VillaList = JsonConvert.DeserializeObject<List<VillaDTO>>
+                (Convert.ToString(response.Result)).Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            }); ;
+        }
+        return View(villaNumberVM);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> CreateVillaNumber(VillaNumberCreateDTO model)
+    {
+        if (ModelState.IsValid)
+        {
+
+            var response = await _villaNumberService.CreateAsync<APIResponse>(model);
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(IndexVillaNumber));
+            }
+        }
+        return View(model);
     }
 }
