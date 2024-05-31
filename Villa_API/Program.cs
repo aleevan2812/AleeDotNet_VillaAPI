c67c498ea1fead12ff6184d5d28b79eed4a4e6f0
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-05-31 10:11:39 +0700

    10 - 002 Add Versioning to API Services

--- Villa_API/Program.cs
+++ Villa_API/Program.cs
@@ -6,6 +6,7 @@ using Villa_API.Repository;
 using Villa_API.Repository.IRepository;
 using AleeDotNet_VillaNumberAPI.Repository;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -19,6 +20,12 @@ builder.Services.AddScoped<IVillaRepository, VillaRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IVillaNumberRepository, VillaNumberRepository>();
 builder.Services.AddAutoMapper(typeof(MappingConfig));
+
+// add versioning to services
+builder.Services.AddApiVersioning(options => {
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+});
 
 var key = builder.Configuration.GetValue<string>("ApiSettings:Secret");
 
