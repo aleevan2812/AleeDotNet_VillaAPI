b6ef3bce3124d0bd631ad4df765de1dd3974faad
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-05-31 11:37:29 +0700

    10 - 006 Swagger Document for v1

--- Villa_API/Program.cs
+++ Villa_API/Program.cs
@@ -99,6 +99,24 @@ builder.Services.AddSwaggerGen(options =>
         }
     });
     
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1.0",
+        Title = "Alee Villa",
+        Description = "API to manage Villa",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Alee-Contact",
+            Url = new Uri("https://contact.com")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+    
 });
 
 var app = builder.Build();
@@ -107,7 +125,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Alee_VillaV1");
+    });
 }
 
 app.UseHttpsRedirection();
