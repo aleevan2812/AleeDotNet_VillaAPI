408e1333f575f157644406409fbe9b3d6ddfa212
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-05-31 15:08:55 +0700

    11 - 008 Bug Fixing - set default pageSize, pageNumber

--- Villa_API/Controllers/v1/VillaAPIController.cs
+++ Villa_API/Controllers/v1/VillaAPIController.cs
@@ -44,7 +44,7 @@ public class VillaAPIController : ControllerBase // dont need Controller Class
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<ActionResult<APIResponse>> GetVillas([FromQuery(Name = "filterOccupancy")] int? occupancy,
-        [FromQuery] string? search, int pageSize = 2, int pageNumber = 1)
+        [FromQuery] string? search, int pageSize = 0, int pageNumber = 1)
     {
         try
         {
