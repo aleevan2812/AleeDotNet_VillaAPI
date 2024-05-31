491a64194e70956c9534042ca5ff8536e08a7919
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-05-31 16:59:40 +0700

    12 - .NET Identity_2

--- Villa_API/Repository/UserRepository.cs
+++ Villa_API/Repository/UserRepository.cs
@@ -1,6 +1,8 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using Villa_API.Data;
 using Villa_API.Models;
@@ -12,17 +14,24 @@ namespace Villa_API.Repository;
 public class UserRepository : IUserRepository
 {
     private readonly ApplicationDbContext _db;
-    private string secretKey;
+    private readonly IMapper _mapper;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly string secretKey;
 
-    public UserRepository(ApplicationDbContext db, IConfiguration configuration)
+    public UserRepository(ApplicationDbContext db, IConfiguration configuration,
+        UserManager<ApplicationUser> userManager, IMapper mapper, RoleManager<IdentityRole> roleManager)
     {
         _db = db;
+        _mapper = mapper;
+        _userManager = userManager;
         secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+        _roleManager = roleManager;
     }
 
     public bool IsUniqueUser(string username)
     {
-        var user = _db.LocalUsers.FirstOrDefault(x => x.UserName == username);
+        var user = _db.ApplicationUsers.FirstOrDefault(x => x.UserName == username);
         if (user == null) return true;
 
         return false;
@@ -30,18 +39,20 @@ public class UserRepository : IUserRepository
 
     public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
     {
-        var user = _db.LocalUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDTO.UserName.ToLower()
-                                                      && u.Password == loginRequestDTO.Password);
+        // var user = _db.LocalUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDTO.UserName.ToLower()
+        //                                               && u.Password == loginRequestDTO.Password);
+        var user = _db.ApplicationUsers
+            .FirstOrDefault(u => u.UserName.ToLower() == loginRequestDTO.UserName.ToLower());
 
-        if (user == null)
-        {
-            return new LoginResponseDTO()
+        bool isValid = await _userManager.CheckPasswordAsync(user, loginRequestDTO.Password);
+        if (user == null || isValid == false)
+            return new LoginResponseDTO
             {
                 Token = "",
                 User = null
             };
-        }
 
+        var roles = await _userManager.GetRolesAsync(user);
         /* if user was found generate JWT Token */
         var tokenHandler = new JwtSecurityTokenHandler();
         // secretKey là khóa bí mật dùng để ký JWT token, được mã hóa thành byte array
@@ -52,40 +63,56 @@ public class UserRepository : IUserRepository
             // Subject: Chứa các thông tin xác nhận (claims) về người dùng,
             Subject = new ClaimsIdentity(new Claim[]
             {
-                new Claim(ClaimTypes.Name, user.Id.ToString()),
-                new Claim(ClaimTypes.Role, user.Role)
+                new(ClaimTypes.Name, user.UserName),
+                new(ClaimTypes.Role, roles.FirstOrDefault())
             }),
             // Expires: Thời hạn của token, ở đây là 7 ngày kể từ thời điểm tạo.
             Expires = DateTime.UtcNow.AddDays(7),
             // SigningCredentials: Chứa thông tin về phương thức ký token, sử dụng thuật toán HMAC SHA256 với khóa đối xứng (SymmetricSecurityKey).
-            SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            SigningCredentials =
+                new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
         };
 
         // Tạo JWT token
         var token = tokenHandler.CreateToken(tokenDescriptor);
         // Tạo đối tượng LoginResponseDTO chứa token và thông tin người dùng, sau đó trả về đối tượng này.
-        LoginResponseDTO loginResponseDTO = new LoginResponseDTO()
+        LoginResponseDTO loginResponseDTO = new LoginResponseDTO
         {
             // Viết token thành chuỗi (tokenHandler.WriteToken(token)).
             Token = tokenHandler.WriteToken(token),
-            User = user
+            User = _mapper.Map<UserDTO>(user)
+            // Role = roles.FirstOrDefault()
         };
         return loginResponseDTO;
     }
 
-    public async Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+    public async Task<UserDTO> Register(RegisterationRequestDTO registerationRequestDTO)
     {
-        LocalUser user = new()
+        ApplicationUser user = new()
         {
             UserName = registerationRequestDTO.UserName,
-            Password = registerationRequestDTO.Password,
-            Name = registerationRequestDTO.Name,
-            Role = registerationRequestDTO.Role
+            Name = registerationRequestDTO.Name
         };
 
-        _db.LocalUsers.Add(user);
-        await _db.SaveChangesAsync();
-        user.Password = "";
-        return user;
+        try
+        {
+            var result = await _userManager.CreateAsync(user, registerationRequestDTO.Password);
+            if (result.Succeeded)
+            {
+                if (!_roleManager.RoleExistsAsync("admin").GetAwaiter().GetResult()){
+                    await _roleManager.CreateAsync(new IdentityRole("admin"));
+                    await _roleManager.CreateAsync(new IdentityRole("customer"));
+                }
+                await _userManager.AddToRoleAsync(user, "admin");
+                var userToReturn = _db.ApplicationUsers
+                    .FirstOrDefault(u => u.UserName == registerationRequestDTO.UserName);
+                return _mapper.Map<UserDTO>(userToReturn);
+            }
+        }
+        catch (Exception e)
+        {
+        }
+
+        return new UserDTO();
     }
 }
