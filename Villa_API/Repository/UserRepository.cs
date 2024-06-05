6a884b3b946af9ac2a896bd7b864c15ab0707c90
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-06-05 15:41:18 +0700

    06 - 004 Get New Refresh Token and Sign in User

--- Villa_API/Repository/UserRepository.cs
+++ Villa_API/Repository/UserRepository.cs
@@ -177,7 +177,7 @@ public class UserRepository : IUserRepository
             }),
             // Expires: Thời hạn của token, ở đây là 7 ngày kể từ thời điểm tạo.
             // Expires = DateTime.UtcNow.AddDays(7),
-            Expires = DateTime.UtcNow.AddMinutes(1),
+            Expires = DateTime.UtcNow.AddMinutes(3),
             // SigningCredentials: Chứa thông tin về phương thức ký token, sử dụng thuật toán HMAC SHA256 với khóa đối xứng (SymmetricSecurityKey).
             SigningCredentials =
                 new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
@@ -213,7 +213,7 @@ public class UserRepository : IUserRepository
             UserId = userId,
             JwtTokenId = tokenId,
             // ExpiresAt = DateTime.UtcNow.AddDays(30),
-            ExpiresAt = DateTime.UtcNow.AddMinutes(3),
+            ExpiresAt = DateTime.UtcNow.AddMinutes(5),
             Refresh_Token = Guid.NewGuid() + "-" + Guid.NewGuid()
         };
 
