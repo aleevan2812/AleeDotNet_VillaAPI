85069ce00fdbb4bc5443fdc097f37083c671c82d
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-06-05 15:31:44 +0700

    05 - 012 Execute Update NET 7 Feature

--- Villa_API/Repository/UserRepository.cs
+++ Villa_API/Repository/UserRepository.cs
@@ -115,13 +115,15 @@ public class UserRepository : IUserRepository
         /*When someone tries to use not valid refresh token, fraud possible*/
         if (!existingRefreshToken.IsValid)
         {
-            var chainRecords = _db.RefreshTokens.Where(u => u.UserId == existingRefreshToken.UserId
-                                                            && u.JwtTokenId == existingRefreshToken.JwtTokenId);
+            // .NET 8
+            var chainRecords = await _db.RefreshTokens.Where(u => u.UserId == existingRefreshToken.UserId
+                                                                  && u.JwtTokenId == existingRefreshToken.JwtTokenId)
+                .ExecuteUpdateAsync(u => u.SetProperty(refreshToken => refreshToken.IsValid, false));
 
-            foreach (var item in chainRecords) item.IsValid = false;
-
-            _db.UpdateRange(chainRecords);
-            _db.SaveChanges();
+            // .NET 7
+            // foreach (var item in chainRecords) item.IsValid = false; 
+            // _db.UpdateRange(chainRecords);
+            // _db.SaveChanges();
             return new TokenDTO();
         }
 
