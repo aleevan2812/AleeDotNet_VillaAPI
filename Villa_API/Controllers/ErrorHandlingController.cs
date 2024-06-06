238a29c1acc85711906001678e820a4bf8664c39
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-06-06 11:45:35 +0700

    09 - 003 Custom Problem Implementation

--- Villa_API/Controllers/ErrorHandlingController.cs
+++ Villa_API/Controllers/ErrorHandlingController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Villa_API.Controllers;
@@ -11,8 +12,20 @@ namespace Villa_API.Controllers;
 public class ErrorHandlingController : ControllerBase
 {
     [Route("ProcessError")]
-    public IActionResult ProcessError()
+    public IActionResult ProcessError([FromServices] IHostEnvironment hostEnvironment)
     {
+        if (hostEnvironment.IsDevelopment())
+        {
+            //custom logic
+            var feature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            return Problem(
+                feature.Error.StackTrace,
+                title: feature.Error.Message,
+                instance: hostEnvironment.EnvironmentName
+            );
+        }
+
         return Problem();
     }
 }
